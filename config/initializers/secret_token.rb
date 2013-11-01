cce88bff0f0da111b3d39a58618bcc04434fad08
Anton Shabanov <me.antony.rain@gmail.com>
2013-11-01 17:12:25 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skydev::Application.config.secret_token = '7db1d2fab4ae61238a0e3b4c6ba9a5c6d733befabd185107fc2c0032f479925ffd8e83b3aca7f50e2b4e256a3b425defe303e520ea63709e1fc50c133df3d698'
